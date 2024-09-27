0d4128269bd36c658d59cb377db1d2f22c28cf3e
T'Cheznavia Cherry <chezcye@gmail.com>
2024-09-27 19:13:47 -0400

    Add files via upload

---
+++ Code_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_TriviaApp.swift
+//  Code Trivia
+//
+//  Created by T'Cheznavia  Cherry on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Code_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WelcomeView()
+        }
+    }
+}
